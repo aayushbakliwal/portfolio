11cdf3f20a4d86c20f7512818aa547485a91e5aa
aayush jain <aayushbakliwal.2625@gmail.com>
2019-11-25 09:40:31 +0530

    made an menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,11 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-NAB");
+const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-items");
+
 let showMenu = false;
+
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
@@ -17,10 +19,10 @@ function toggleMenu() {
     //set Menu state;
     showMenu = true;
   } else {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuBranding.classList.add("show");
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
     //set Menu state;
